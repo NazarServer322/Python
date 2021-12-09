d8ea53c85e404d5a4a9cd50d81df5f5341012d82
Nazar Mulko <Nazarbukmeker@gmail.com>
2021-12-09 16:17:37 +0200

    example

--- parapico.py
+++ parapico.py
@@ -8,7 +8,11 @@ try:
     sshcon.connect(hostname, username=myuser, key_filename=mySSHK)
 except Exception as error:
     print(error) 
-stdin, stdout, stderr = sshcon.exec_command("cd ~/Nazar && pwd && mkdir testdir && cd /testdir && ls")
+# stdin, stdout, stderr = sshcon.exec_command("cd ~/Nazar && pwd && mkdir testdir && cd ~/testdir && pwd ")
+# result = stdout.read().splitlines()
+# print(result)
+stdin, stdout, stderr = sshcon.exec_command("cd ~/Nazar && pwd && > file.txt && echo * hope this gonna work* >> file.txt && cat file.txt && rm file.txt && sudo reboot now ")
 result = stdout.read().splitlines()
 print(result)
+
 
